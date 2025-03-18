1f4639ba68ecbdab469ecc97983ad9409486c89d
Juan_Arturo <juanpabloarturoazcarate2@gmail.com>
2025-03-18 14:22:55 -0500

    actualizacion header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
-import "../styles/header.css";
+import "../styles/Header.css";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
